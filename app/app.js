6997ac28b3de97212dce03b649c233badb7f327e
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-02-27 13:44:30 +0700

    Added residents.

--- app/app.js
+++ app/app.js
@@ -16,7 +16,8 @@ app.use(express.static(__dirname + '/assets'));
 
 app.get('/', function(req, res) {
  	res.render('index', {
-  		_layoutFile: 'layout'
+  		_layoutFile: 'layout',
+  		residents: require('./residents.json')
   	})
 });
 
